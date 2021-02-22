0075b1cdae046604b376ba43f3d7bfc7b87ad208
Viola8905 <74563100+Viola8905@users.noreply.github.com>
2021-02-22 21:18:47 +0200

    Add files via upload

---
+++ lab5.2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace лабка_5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("x=");
+            int X = Convert.ToInt32(Console.ReadLine());
+            int k;
+
+            k = X % 10;
+            X = X / 10;
+
+
+
+            while (X > 0)
+            {
+                k = Math.Max(k, X % 10);
+                X = X / 10;
+
+            }
+            Console.WriteLine(k);
+
+
+            
+        }
+    }
+}
---
+++ lab5.4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lab_5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fi=0, fi_1=0, fi_2,fi_3;
+            fi_2=fi_3 = 9;
+            Console.WriteLine("n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 3; i <= n; i++)
+            {
+                fi = fi_3 + fi_2 + fi_1;
+                fi_3 = fi_2;
+                fi_2 = fi_3;
+                fi_3 = fi;
+            }
+            Console.WriteLine($"f{n}={fi}"); 
+
+        }
+    }
+}
